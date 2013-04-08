88ea1cc3808a19866ef9f757cea80bbb6f1277b1
David Lee <david.lee@bookt.com>
2013-04-08 17:07:33 -0400

    Footer Widget Updated
    Classnames Added, structure updated so it can be styled better

--- insta-default-content/insta-footer.php
+++ insta-default-content/insta-footer.php
@@ -1,8 +1,10 @@
-<div id="footer">
-	<span id="copyright">&copy;{{site.SolutionCopyright}}</span>&nbsp;&nbsp;|&nbsp;&nbsp;
-	<span id="terms"><a href="/termsofuse" target="_blank">{{#textdata.Terms}}{{textdata.Terms}}{{/textdata.Terms}}{{^textdata.Terms}}Terms{{/textdata.Terms}}</a></span>&nbsp;&nbsp;|&nbsp;&nbsp;
-	<span id="privacy"><a href="/privacypolicy" target="_blank">{{#textdata.Privacy}}{{textdata.Privacy}}{{/textdata.Privacy}}{{^textdata.Privacy}}Privacy{{/textdata.Privacy}}</a></span>&nbsp;&nbsp;|&nbsp;&nbsp;
-	<span id="applogin"><a rel="nofollow" href="https://{{site.App.Data}}">{{site.App.Label}}</a></span>
-	<div class="clear"></div>
-	<span id="poweredby"><a rel="nofollow" href="http://{{site.PoweredBy.Data}}">{{site.PoweredBy.Label}}</a></span>	
+<div class="footer">
+	<span class="copyright">&copy;{{site.SolutionCopyright}}</span>
+    
+    
+    <div class="footer-links">
+    <span class="poweredby"><a rel="nofollow" href="http://{{site.PoweredBy.Data}}">{{site.PoweredBy.Label}}</a></span>&nbsp;&nbsp;|&nbsp;&nbsp;<span class="applogin"><a rel="nofollow" href="{{site.App.Data}}">{{site.App.Label}}</a></span>&nbsp;&nbsp;|&nbsp;&nbsp;<span class="terms"><a href="/termsofuse" target="_blank">{{#textdata.Terms}}{{textdata.Terms}}{{/textdata.Terms}}{{^textdata.Terms}}Terms{{/textdata.Terms}}</a></span>&nbsp;&nbsp;|&nbsp;&nbsp;<span class="privacy"><a href="/privacypolicy" target="_blank">{{#textdata.Privacy}}{{textdata.Privacy}}{{/textdata.Privacy}}{{^textdata.Privacy}}Privacy{{/textdata.Privacy}}</a></span>
+	</div>
+	
+<div class="clear"></div>		
 </div>
