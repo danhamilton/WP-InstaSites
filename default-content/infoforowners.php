693be1034cdd206a36fddebde30656b112fea4d5
David Lee <david.lee@bookt.com>
2013-06-03 16:48:15 -0400

    Updated Default Static Pages
    Added 3 new Default Static Pages and updated 2.

---
+++ default-content/infoforowners.php
@@ -0,0 +1,16 @@
+<h1>Information for Our Owners</h1>
+
+<h3>Welcome to our Online Home!</h3>
+<p>Thank you for considering us to manage your rental property.  We have built our reputation on providing quality rentals with superior customer services, so congratulations – you have just taken the first step to a profitable business relationship.</p>
+
+<h3>Savvy Marketing</h3>
+<p>Both Property Owners and Renters like that we advertise our rentals beyond just this website, and use a multi-channel approach and also list on many of the popular industry portals. This allows us to reach more renters with our message and better keep our units booked, including yours.  Another key aspect of this business is the ability to get to know our guests in order to provide them with customized service leading to greater customer satisfaction so they will vacation with us again and again.  That's another way we get the highest occupancy levels and maintain well-cared for properties for owners.</p>
+
+<h3>Latest Technology</h3>
+<p>We have made significant investments in our technology, to ensure a smooth booking and stay experience for our guests, and an efficient and accountable process for our property owners. Examples are this website which can be accessed effectively regardless if the user has a mobile phone, tablet or a desktop computer. We also have an easy to use search and booking engine, which will showcase your properties with large attractive photos. All guest communication is standardized for speed and accuracy, which means more bookings for our home owners.</p>
+
+<h3>Excellence and Integrity</h3>
+<p>We are laser-focused on serving owners with excellence and integrity.  Our formula for success?  We know that you have choices and want to both protect and maximize your investment in your vacation home.  That's why we have put together a knowledgeable and experienced team that delivers the best in property care and cleaning, cost-saving measures, and a savvy marketing program to get you the occupancy levels you are looking for.  We also have standardized processes to give you timely financial statements and rent payments.</p>
+
+<h3>Please Connect Today!</h3>
+<p>As we always say, our specialty is renting the right home to the right guest.  Let's talk about your property and your goals and see if our approach and experience is right for you.  Call us or email us today!  We look forward to hearing from you and delivering a level of service exceeding your expectations.</p>
