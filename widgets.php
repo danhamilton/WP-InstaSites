d373d0bbe52f199a9c28b9090c0122d8760de310
David Lee <david.lee@bookt.com>
2013-06-10 14:06:05 -0400

    Mantis Ticket: 0003852
    Removed title from mustache template. the title is handled by the widget
    title option in the widgets page now, updated contact us page default
    template

--- widgets.php
+++ widgets.php
@@ -368,7 +368,7 @@ class BAPI_Inquiry_Form extends WP_Widget {
 
 		echo $before_widget;
 		if ( ! empty( $title ) )
-			//echo $before_title . $title . $after_title;
+			echo $before_title . $title . $after_title;
 		?>
 		<div id="bapi-inquiryform" class="bapi-inquiryform" data-templatename="tmpl-leadrequestform-propertyinquiry" data-log="0"></div>        
         <?php
@@ -607,7 +607,6 @@ class BAPI_Similar_Properties extends WP_Widget {
 		extract($args);
 		$title = apply_filters('widget_title',$instance['title']);
 		$pagesize = esc_textarea($instance['text']);
-		if(empty($pagesize)) { $pagesize = 3; }
 		$rowsize = intval($instance['rowsize']);
 		if($rowsize<=0) { $rowsize=1; }
 		
@@ -615,7 +614,7 @@ class BAPI_Similar_Properties extends WP_Widget {
 		if(!empty($title))
 			echo $before_title.$title.$after_title;
 		?>
-        <div id="featuredproperties" class="bapi-summary" data-log="0" data-templatename="tmpl-featuredproperties-quickview" data-entity="property" data-searchoptions='{ "pagesize": <?= $pagesize ?>, "sort": "random", "similarto": true }' data-rowfixselector=".fp-featured" data-rowfixcount="<?= $rowsize ?>"></div>
+        <div id="featuredproperties" class="bapi-summary" data-log="0" data-templatename="tmpl-featuredproperties-quickview" data-entity="property" data-searchoptions='{ "pagesize": <?= $pagesize ?>, "sort": "random" }' data-rowfixselector=".fp-featured" data-rowfixcount="<?= $rowsize ?>"></div>
 		<?php
 		echo $after_widget;
 	}
