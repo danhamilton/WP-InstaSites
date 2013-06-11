c82211cb1b9527a023db32c67bcf04597945a6cd
Robert Brown <bob@bookt.com>
2013-06-11 12:09:44 -0400

    make payment screen "temporarily..

--- default-content/makepayment.php
+++ default-content/makepayment.php
@@ -1,1 +1,4 @@
 <div id="bookingform" class="bapi-bookingform"></div>
+<h1>Booking Payment Page</h1>
+
+<p>We are sorry but this page is temporarily down.  Please <a href="{{site.PrimaryEmail}}">contact us.</a> to make a payment</p>
