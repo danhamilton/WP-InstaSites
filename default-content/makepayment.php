9b2083b0abee9a8ade1a73020592f7c4cc047a12
Robert Brown <bob@bookt.com>
2013-06-26 14:22:49 -0400

    test

--- default-content/makepayment.php
+++ default-content/makepayment.php
@@ -1,4 +1,4 @@
-<h1>Booking Payment Page Hello</h1>
+<h1>Booking Payment Page</h1>
 
 <p>We are sorry but this page is temporarily down.  Please <a href="{{site.PrimaryEmail}}">contact us.</a> to make a payment</p>
 
