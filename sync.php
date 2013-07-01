d16b0229d197166dd12a7a5ae6a49f6e5b70665f
Jacob Baron <jacob@bookt.com>
2013-07-01 00:48:15 -0400

    do not refresh cache from dashboard pages

--- sync.php
+++ sync.php
@@ -211,49 +211,51 @@
 	
 	function bapi_sync_coredata() {
 		// initialize the bapisync object
-		global $bapisync;
-		$bapisync = new BAPISync();
-		$bapisync->init();
-		
-		$bapi = getBAPIObj();
-		if (!$bapi->isvalid()) { return; }
-		
-		// check if we need to refresh textdata
-		$data = BAPISync::getTextDataRaw();
-		$lastmod = BAPISync::getTextDataLastModRaw();
-		if(empty($data) || empty($lastmod) || ((time()-$lastmod)>3600)) {					
-			$data = $bapi->gettextdata(true);			
-			if (!empty($data)) {
-				$data = $data['result']; // just get the result part
-				$data = json_encode($data); // convert back to text
-				update_option('bapi_textdata',$data);
-				update_option('bapi_textdata_lastmod',time());
-			}					
-		}	
-		
-		// check if we need to refresh solution data
-		$data = BAPISync::getSolutionDataRaw();
-		$lastmod = BAPISync::getSolutionDataLastModRaw();
-		if(empty($data) || empty($lastmod) || ((time()-$lastmod)>3600)) {					
-			$data = $bapi->getcontext(true);
-			if (!empty($data)) {
-				$data = json_encode($data); // convert back to text
-				update_option('bapi_solutiondata',$data);
-				update_option('bapi_solutiondata_lastmod',time());
-			}					
-		}	
-		
-		// check if we need to refresh seo data
-		$data = BAPISync::getSEODataRaw();
-		$lastmod = BAPISync::getSEODataLastModRaw();
-		if(empty($data) || empty($lastmod) || ((time()-$lastmod)>3600)) {					
-			$data = $bapi->getseodata(true);
-			if (!empty($data)) {
-				$data = $data['result']; // just get the result part
-				$data = json_encode($data); // convert back to text
-				update_option('bapi_keywords_array',$data);
-				update_option('bapi_keywords_lastmod',time());
-			}					
+		if(strpos($_SERVER['PATH_INFO'],'wp-admin')===false){			
+			global $bapisync;
+			$bapisync = new BAPISync();
+			$bapisync->init();
+			
+			$bapi = getBAPIObj();
+			if (!$bapi->isvalid()) { return; }
+			
+			// check if we need to refresh textdata
+			$data = BAPISync::getTextDataRaw();
+			$lastmod = BAPISync::getTextDataLastModRaw();
+			if(empty($data) || empty($lastmod) || ((time()-$lastmod)>3600)) {					
+				$data = $bapi->gettextdata(true);			
+				if (!empty($data)) {
+					$data = $data['result']; // just get the result part
+					$data = json_encode($data); // convert back to text
+					update_option('bapi_textdata',$data);
+					update_option('bapi_textdata_lastmod',time());
+				}					
+			}	
+			
+			// check if we need to refresh solution data
+			$data = BAPISync::getSolutionDataRaw();
+			$lastmod = BAPISync::getSolutionDataLastModRaw();
+			if(empty($data) || empty($lastmod) || ((time()-$lastmod)>3600)) {					
+				$data = $bapi->getcontext(true);
+				if (!empty($data)) {
+					$data = json_encode($data); // convert back to text
+					update_option('bapi_solutiondata',$data);
+					update_option('bapi_solutiondata_lastmod',time());
+				}					
+			}	
+			
+			// check if we need to refresh seo data
+			$data = BAPISync::getSEODataRaw();
+			$lastmod = BAPISync::getSEODataLastModRaw();
+			if(empty($data) || empty($lastmod) || ((time()-$lastmod)>3600)) {					
+				$data = $bapi->getseodata(true);
+				if (!empty($data)) {
+					$data = $data['result']; // just get the result part
+					$data = json_encode($data); // convert back to text
+					update_option('bapi_keywords_array',$data);
+					update_option('bapi_keywords_lastmod',time());
+				}					
+			}
 		}	
 	}	
 ?>
