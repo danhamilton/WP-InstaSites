369efb2144ac44ab9bd9e000e26ad030356b72fc
Jacob Baron <jacob@bookt.com>
2013-10-18 18:07:16 -0400

    bapi.init not getting hit for signup/sync/default-pages

--- bookt-api.php
+++ bookt-api.php
@@ -50,6 +50,7 @@ add_action('wp_head','bapi_getmeta',1);
 add_action('wp_head','display_global_header',2);
 add_action('init','bapi_create_site',1);  //Hook to add new sites
 add_action('init','bapi_setup_default_pages',1);
+add_action('init','urlHandler_bapidefaultpages',1);
 add_action('init','urlHandler_securepages',1);  //Hook to force redirect to secure pages
 add_action('init','bapi_wp_site_options',1);  //Preload Site Data to help reduce DB usage
 add_action('init','bapi_sync_coredata',2); 	// syncing BAPI core data
