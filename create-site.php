bb880c76429a3a82fac4c2ad4c625d0ff7fb4fed
Jacob Baron <jacob@bookt.com>
2013-11-15 11:12:46 -0500

    signup

--- create-site.php
+++ create-site.php
@@ -55,10 +55,11 @@ function bapi_create_site(){
 		exit();
 	}
 	$meta = array('api_key' => $apikey, 'bapi_secureurl' => $prefix.'.imbookingsecure.com', 'bapi_site_cdn_domain' => $liveurl, 'bapi_cloudfronturl' => $cf, 'blogdescription' => $tagline, 'bapi_first_look' => 1);
-	if(defined(BAPI_BASEURL) && (BAPI_BASEURL == 'connect.bookt.biz')){
+	if(defined('BAPI_BASEURL') && (BAPI_BASEURL == 'connect.bookt.biz')){
 		$meta['bapi_secureurl'] = $prefix.'.lodgingcloud.com';
 		$meta['bapi_baseurl'] = BAPI_BASEURL;
 	}
+	print_r($meta); exit();
 	//$siteurl = $prefix.'.imbookingsecure.com';
 	
 	$u = username_exists($username);
