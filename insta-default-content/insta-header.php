7f87013798c17e76501434f2171c34b35f79c0de
David Lee <david.lee@bookt.com>
2013-12-02 18:43:07 -0400

    Mantis Ticket 0005025

--- insta-default-content/insta-header.php
+++ insta-default-content/insta-header.php
@@ -29,5 +29,10 @@
 <script type="text/javascript">
 	$(document).ready(function () {
 		BAPI.UI.createCurrencySelectorWidget('.currencyselector');
+		$( '.siteselector .flag' ).each(function(i) {
+			var theClassnames = $(this).attr('class');
+			theClassnames = theClassnames.substring(0, theClassnames.length - 3);
+			$(this).addClass(theClassnames);
+		});
 	});
 </script>
