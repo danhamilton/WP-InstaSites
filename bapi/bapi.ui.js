ff4a60e1f33eeaa6d601e7dac2e6ec2a2a0f72f1
Robert Brown <bob@bookt.com>
2013-12-23 11:48:20 -0500

    http://mantis.bookt.com/view.php?id=4919

--- bapi/bapi.ui.js
+++ bapi/bapi.ui.js
@@ -1626,7 +1626,7 @@ function loadFormFromSession(s) {
 	$('.sessionbedsmin').val(s.beds.min);
 	$('.sessionbathsmin').val(s.baths.min);
 	$('.sessionlocation').val(s.location);
-	$('.sessionratemin').val(s.rate.min);
+	$('.sessionratemin').val(s.rate.max);
 	$('.sessionroomsmin').val(s.rooms.min);
 	$('.sessionheadline').val(s.headline);
 	$('.sessionaltid').val(s.altid);
