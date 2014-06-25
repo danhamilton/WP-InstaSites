b5ab3b96e62f8b12f7b097b43c15462dba405d24
juan-bookt <juan@bookt.com>
2014-06-25 16:34:27 -0400

    Revert "Mantis Ticket: 005045 user can now edit title of search bucket."
    This reverts commit b13f6e2c3f3ad27a936b4475cce973dc878bf192.

--- shortcodes.php
+++ shortcodes.php
@@ -47,8 +47,8 @@ add_shortcode( 'allproperties', 'all_properties_sc' );
 
 
 //Predefined Searches
-function predefined_searches_sc($atts, $content){
-	$d = '<header class="pagination-centered"><h1>'.$content.'</h1></header><div id="results" class="propertyfinders-results bapi-summary" data-log="0" data-templatename="tmpl-searches-summary" data-entity="searches" data-rowfixselector=".pfrowfix" data-rowfixcount="3"></div>';
+function predefined_searches_sc(){
+	$d = '<div id="results" class="propertyfinders-results bapi-summary" data-log="0" data-templatename="tmpl-searches-summary" data-entity="searches" data-rowfixselector=".pfrowfix" data-rowfixcount="3"></div>';
 	return $d;
 }
 add_shortcode( 'predefined-searches', 'predefined_searches_sc' );
