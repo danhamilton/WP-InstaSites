c2c9cfd7982b3532dfe8ce07d09fb76df5725433
juan-bookt <juan@bookt.com>
2014-06-25 13:57:36 -0400

    Hot Fix:0005045 added correct default title  to search buckets
    added correct default title  to search buckets

--- shortcodes.php
+++ shortcodes.php
@@ -48,6 +48,9 @@ add_shortcode( 'allproperties', 'all_properties_sc' );
 
 //Predefined Searches
 function predefined_searches_sc($atts, $content){
+	if($content == ""){
+			$content = "Search Buckets"; 	
+	}
 	$d = '<header class="pagination-centered"><h1>'.$content.'</h1></header><div id="results" class="propertyfinders-results bapi-summary" data-log="0" data-templatename="tmpl-searches-summary" data-entity="searches" data-rowfixselector=".pfrowfix" data-rowfixcount="3"></div>';
 	return $d;
 }
