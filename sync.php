b9852681fb3ca1d6ebe70378cdbd2eaa35a1dd91
Alexei Matyushkin <am@mudasobwa.ru>
2015-01-09 11:54:52 +0100

    Fixed both Inactive and “Do not show on site”.

--- sync.php
+++ sync.php
@@ -146,6 +146,13 @@
 			}
 
 			$c = $bapi->get($entity,$pkid,$options,true,$debugmode);
+			if( // check the response
+				isset( $c['error'] ) || $c['status'] != '1' ||                                                 // error returned
+				!is_array( $c['result'] ) || count( $c['result'] ) < 1 || !is_array( $c['result'][0] ) ||      // result is corrupted
+				!array_key_exists( 'AvailableOnline', $c['result'][0] ) || !$c['result'][0]['AvailableOnline'] // “Do not show on site” is set
+			) {
+				return false;
+			}
 			$c["config"] = BAPISync::getSolutionData();
 			$c["config"] = $c["config"]["ConfigObj"];
 			/* we get the sitesettings */
@@ -376,11 +383,13 @@
 			// do page update
 			$post->comment_status = "close";		
 			$template = $bapisync->getMustacheTemplate($seo["entity"]);		
-			$post->post_content = $bapisync->getMustache($seo["entity"],$seo["pkid"],$template,$debugmode);
-			//print_r($post); exit();
-			//if we have a bapikey
-			if(!empty($pagekey) || $pagekey != null){
-				$dom = new DomDocument();
+			if( !is_string( $s2s_success = $bapisync->getMustache( $seo["entity"], $seo["pkid"], $template, $debugmode ) ) ) {
+				$post->post_status = 'trash';
+			} else {
+				$post->post_content = $s2s_success;
+				//if we have a bapikey
+				if(!empty($pagekey) || $pagekey != null){
+				   $dom = new DomDocument();
 				   libxml_use_internal_errors(true);
 				   $dom->loadHTML("<!DOCTYPE html><html><head></head><body>".$post->post_content."</body></html>");
 				   libxml_use_internal_errors(false);
@@ -400,16 +409,17 @@
 					 $page_title = $entity_title[0];
 					}
 				   }
+				}
+
+				$post->post_title = $page_title;
+				$post->post_name = BAPISync::clean_post_name($seo["DetailURL"]);
+				$post->post_parent = get_page_by_path(BAPISync::getRootPath($seo["entity"]))->ID;
+				if($do_market_update){
+					$post->post_parent = ensure_ma_landing_pages($seo["DetailURL"]);
+				}
+				$post->post_type = "page";
+				remove_filter('content_save_pre', 'wp_filter_post_kses');
 			}
-			
-			$post->post_title = $page_title;
-			$post->post_name = BAPISync::clean_post_name($seo["DetailURL"]);
-			$post->post_parent = get_page_by_path(BAPISync::getRootPath($seo["entity"]))->ID;
-			if($do_market_update){
-				$post->post_parent = ensure_ma_landing_pages($seo["DetailURL"]);
-			}
-			$post->post_type = "page";
-			remove_filter('content_save_pre', 'wp_filter_post_kses');
 			//print_r($post);exit();
 			if (empty($post->ID)) {
 				$post->ID = wp_insert_post($post, $wp_error);
